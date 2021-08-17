2a49f429752abfcad93613c629ab548ed25cc416
sgrunske <sgrunske@cisco.com>
2021-08-17 14:28:20 +0200

    modules/terraform-intersight-iks/versions:

--- modules/terraform-intersight-iks/versions.tf
+++ modules/terraform-intersight-iks/versions.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_version = ">=0.14.5"
-
-  required_providers {
-    intersight = {
-      source  = "CiscoDevNet/intersight"
-      version = "=1.0.11"
-    }
-  }
+#terraform {
+#  required_version = ">=0.14.5"
+#
+#  required_providers {
+#    intersight = {
+#      source  = "CiscoDevNet/intersight"
+#      version = "=1.0.11"
+#    }
+#  }
