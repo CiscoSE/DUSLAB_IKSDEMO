239bb15703b3cdc9691ee209a74ec11f71f48aba
sgrunske <sgrunske@cisco.com>
2021-08-18 13:50:28 +0200

    modules/intersight-cluster-iks001/main:

--- modules/intersight-cluster-iks001/main.tf
+++ modules/intersight-cluster-iks001/main.tf
@@ -1,5 +1,5 @@
 
-resource "intersight_kubernetes_cluster_profile" "${var.clusterName}" {
+resource "intersight_kubernetes_cluster_profile" "intersight_k8s_cluster_profile" {
   description = "Terraform deployed K8s Cluster" 
   name        = var.ClusterName
   type        = "instance"
