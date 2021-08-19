44cf09d8cce837a023bfd2b767713a6c975c529c
sgrunske <sgrunske@cisco.com>
2021-08-19 11:07:04 +0200

    modules/intersight-cluster-iks001/main:

--- modules/intersight-cluster-iks001/main.tf
+++ modules/intersight-cluster-iks001/main.tf
@@ -227,3 +227,37 @@ resource "intersight_kubernetes_virtual_machine_infrastructure_provider" "inters
     moid = intersight_kubernetes_virtual_machine_infra_config_policy.intersight_k8s_virtual_machine_infra_config_policy.moid
   }
 }
+
+data "intersight_kubernetes_addon_definition" "this" {
+  for_each = { for addon in var.addons : addon.addon_policy_name => addon }
+  name     = each.value.addon
+}
+# Creating addon Policy
+resource "intersight_kubernetes_addon_policy" "this" {
+
+  for_each    = { for addon in var.addons : addon.addon_policy_name => addon }
+  name        = each.value.addon_policy_name
+  description = each.value.description
+
+  addon_configuration {
+    install_strategy = each.value.install_strategy
+    upgrade_strategy = each.value.upgrade_strategy
+  }
+
+  addon_definition {
+    moid = data.intersight_kubernetes_addon_definition.this[each.key].results.0.moid
+  }
+
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value["key"]
+      value = tags.value["value"]
+    }
+  }
+
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.orgID.results.0.moid
+  }
+}
