a08e12cb6e9a6c0c8b40d4aa31be9465fc35f76c
sgrunske <sgrunske@cisco.com>
2021-08-19 09:42:51 +0200

    main:

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "intersight-cluster-iks001" {
     source = "./modules/intersight-cluster-iks001"
     apikey = var.apikey
     secretkey = var.secretkey
-    vc_password = var.vc_password
+    
     ssh_user = var.ssh_user
     ssh_key = var.ssh_key
     organization = var.organization
